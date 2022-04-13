353f1c89746c0f4faea897a81da9d1745545cd54
cameronmcewan <cm17521@bristol.ac.uk>
2022-04-13 15:56:00 +0100

    added random content and background now shows

--- portfolio/src/routes/Landing.js
+++ portfolio/src/routes/Landing.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../assets/logo-light-no-bg.png';
 
 const Landing = () => {
   return (
-    <div>
+    <>
+    <section>
         <h1>Welcome to Folio!</h1>
         <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia consectetur vero at, porro rerum laudantium, maxime, doloremque ratione doloribus adipisci inventore nesciunt quae nulla corrupti saepe consequuntur modi magni quod.</h3>
-    </div>
+      <img src={Logo} alt="logo" />
+    </section>
+    <section>
+      <h1>More stuff</h1>
+    </section>
+
+    </>
   )
 }
 
