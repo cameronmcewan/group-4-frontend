829a4fc760fa37c980687736a62de8b74fc196f9
kk21812 <97010541+xuexikuaile2@users.noreply.github.com>
2022-04-14 13:48:57 +0100

    Update Navbar.js
    modified the look of Navbar

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,19 +1,30 @@
 import React, {useState} from 'react';
+// import React from 'react';
 import Logo from '../assets/logo-dark-no-bg.png'
 
+import './Navbar.css'
+const jump = (url) =>{
+  window.location.href=url;
+}
 function Navbar() {
-    const[click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+  const[click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
   return (
-    <div>
-        <ul className='topnav'>
-            <a className='active' href="/"><img className='logo' src={Logo} alt="The Folio Logo" /></a>
-            <li><a href="explore">Explore</a></li>
-            <li><a href="create">Create</a></li>
-            <li className='right'><a href="connect">Connect to MetaMask</a></li>
-        </ul>
-    </div>
+    <header>
+      <div className="container header__container">
+        <div className='logo'>
+            <img src={Logo} onClick={jump.bind(this,'/')}></img>
+        </div>
+        <div className='menu'>
+            <ul className='topnav'>
+              <li><a onClick={jump.bind(this,'/explore')}>Explore</a></li>
+              <li><a onClick={jump.bind(this,'/create')}>Create</a></li>
+            </ul>
+        </div>
+        <div className='right_button'>Connect your <br/> MetaMask account</div>
+      </div>
+    </header>
   );
 }
 
-export default Navbar;
+export default Navbar
