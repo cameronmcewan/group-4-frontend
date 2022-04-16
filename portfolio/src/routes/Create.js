4a52b4c28ceb17a41d28b8b7ebe643956b405f09
cameronmcewan <cm17521@bristol.ac.uk>
2022-04-16 21:27:26 +0100

    Added containers to the create page

--- portfolio/src/routes/Create.js
+++ portfolio/src/routes/Create.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import CreateForm from '../components/CreateForm';
+import DropDown from '../components/DropDown';
 
 const Create = () => {
   return (
+    <>
     <section>
-        <h1>Create a new Portfolio</h1>
-        <Container fixed style={{backgroundColor:"skyblue"}}>
-            <h2>This is in the Material UI container</h2>
-        </Container>
+      <header className='block center'>
+        <div>
+          <h1>Create a new portfolio coin</h1>
+        </div>
+      </header>
+      <div className='row'>
+        <h4 className='block col-2'>Your new coin:</h4>
+        <h4 className='block col-1'>
+        Add crypto tokens to your coin:
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam modi laudantium autem adipisci quibusdam provident consequatur, ab porro? Deserunt nam distinctio nesciunt quia quibusdam obcaecati, excepturi ea magnam enim minus. Dolore voluptas excepturi modi. Iste quia consequuntur maxime beatae est!
+        <CreateForm />
+        <DropDown />
+        </h4>
+      </div>
+
+    </section><section>
+        <h1>Another section</h1>
+        <h2>Space</h2>
     </section>
+    </>
 
   )
 }
