42e23719f0108d7cb4bf4ca5da855745141a6543
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-04-18 18:47:43 +0100

    added required state to create page and button to create

--- portfolio/src/routes/Create.js
+++ portfolio/src/routes/Create.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import CreateForm from "../components/CreateForm";
 import DropDown from "../components/DropDown";
+import { UserContext } from "../helpers/UserContext";
+import portfolioFactoryContract from "../contracts/PortfolioFactory";
 
 const Create = () => {
+  const { address } = useContext(UserContext);
+  const [name, setName] = useState("Portfolio");
+  const [symbol, setSymbol] = useState("FOLO");
+  const [tokenAddresses, setTokenAddresses] = useState([]);
+  const [percentageHoldings, setPercentageHoldings] = useState([]);
+  const [ownerFee, setOwnerFee] = useState(0);
+
+  // Example input to create a portfolio
+  // "Portfolio","FOLO", ["0xa36085F69e2889c224210F603D836748e7dC0088", "0xd0A1E359811322d97991E03f863a0C30C2cF029C"],[40, 60],100
+  const createPortfolio = () => {
+    console.log(
+      `The variables are: ${name}, ${symbol}, ${tokenAddresses}, ${percentageHoldings}, ${ownerFee}`
+    );
+    portfolioFactoryContract.methods
+      .create(name, symbol, tokenAddresses, percentageHoldings, ownerFee)
+      .send({
+        from: address,
+      })
+      .then((receipt) => {
+        console.log(receipt);
+      });
+  };
+
   return (
     <>
       <section>
@@ -25,6 +50,12 @@ const Create = () => {
           </h4>
         </div>
       </section>
+      <p>{address}</p>
+      {address && (
+        <button className="btn btn-cta" onClick={createPortfolio}>
+          Create Portfolio
+        </button>
+      )}
       <section>
         <h1>Another section</h1>
         <h2>Space</h2>
