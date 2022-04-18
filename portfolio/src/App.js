4e1d292418c8f4c14509c21c1118d1f0e037e62f
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-04-18 23:21:11 +0100

    fixed routing so that context persists

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,32 +1,22 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Welcome from "./routes/Welcome";
 import Explore from "./routes/Explore";
 import Create from "./routes/Create";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import { UserContext } from "./helpers/UserContext";
 
 function App() {
-
   const [address, setAddress] = useState();
   const userContext = { address, setAddress };
 
   return (
-    <>
     <UserContext.Provider value={userContext}>
       <Navbar />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="explore" element={<Explore />} />
-            <Route path="create" element={<Create />} />
-          </Routes>
-        </Router>
-      {/* </Navbar> */}
+      <Outlet />
+      <Footer />
     </UserContext.Provider>
-    <Footer />
-    </>
   );
 }
 
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './app.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./app.css";
+import App from "./App";
+import Welcome from "./routes/Welcome";
+import Explore from "./routes/Explore";
+import Create from "./routes/Create";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Welcome />} />
+          <Route path="explore" element={<Explore />} />
+          <Route path="create" element={<Create />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
