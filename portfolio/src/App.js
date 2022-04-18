94e4f1f7017007ef619623c0c64f975efdd2bf19
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-04-18 23:16:54 +0100

    fixed routing so that context persists

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Welcome from "./routes/Welcome";
-import Explore from "./routes/Explore";
-import Create from "./routes/Create";
 import Footer from "./components/Footer";
 import { UserContext } from "./helpers/UserContext";
 
@@ -14,14 +11,7 @@ function App() {
   return (
     <UserContext.Provider value={userContext}>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="create" element={<Create />} />
-        </Routes>
-      </Router>
-      {/* </Navbar> */}
+      <Outlet />
       <Footer />
     </UserContext.Provider>
   );
