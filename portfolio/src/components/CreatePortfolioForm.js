22eec7a03eec7a1d2d6e875124ea2cd02bf2a9f1
alex-straw <as17163@bristol.ac.uk>
2022-04-18 21:22:04 +0100

    Created form for making your own portfolio - still more to be done.  I.e., tracking the creation, validation, verification, better css etc

---
+++ portfolio/src/components/CreatePortfolioForm.js
@@ -0,0 +1,88 @@
+import React, { useState, useContext } from "react";
+import portfolioFactoryContract from "../contracts/PortfolioFactory";
+import { UserContext } from "../helpers/UserContext";
+
+const CreatePortfolioForm = () => {
+
+  const { address } = useContext(UserContext);
+  const [name, setName] = useState("Portfolio");
+  const [symbol, setSymbol] = useState("FOLO");
+  const [tokenAddresses, setTokenAddresses] = useState(["0xa36085F69e2889c224210F603D836748e7dC0088", "0xd0A1E359811322d97991E03f863a0C30C2cF029C"]);
+  const [percentageHoldings, setPercentageHoldings] = useState([40, 60]);
+  const [ownerFee, setOwnerFee] = useState(100);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    createPortfolio()
+  }
+
+  const createPortfolio = () => {
+    console.log(
+      `The variables are: ${name}, ${symbol}, ${tokenAddresses}, ${percentageHoldings}, ${ownerFee}`
+    );
+    portfolioFactoryContract.methods
+      .create(name, symbol, tokenAddresses, percentageHoldings, ownerFee)
+      .send({
+        from: address,
+      })
+      .then((receipt) => {
+        console.log(receipt);
+      });
+  };
+
+  return (
+    <form className="formItem" onSubmit={handleSubmit}>
+      <label>
+        Token Name:
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Ticker:
+        <input
+          type="text"
+          value={symbol}
+          onChange={e => setSymbol(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Token Addresses:
+        <input
+          type="text"
+          value={tokenAddresses}
+          onChange={e => setTokenAddresses(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Token Holdings:
+        <input
+          type="text"
+          value={percentageHoldings}
+          onChange={e => setPercentageHoldings(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Owner Fee:
+        <input
+          type="text"
+          value={ownerFee}
+          onChange={e => setOwnerFee(e.target.value)}
+        />
+      </label>
+
+      {address && (
+        <input type="submit" value="Submit" />
+      )}
+
+    </form>
+  );
+};
+
+export default CreatePortfolioForm;
--- portfolio/src/routes/Create.js
+++ portfolio/src/routes/Create.js
@@ -1,32 +1,10 @@
 import React, { useState, useContext } from "react";
-import CreateForm from "../components/CreateForm";
-import DropDown from "../components/DropDown";
+// import portfolioFactoryContract from "../contracts/PortfolioFactory";
 import { UserContext } from "../helpers/UserContext";
-import portfolioFactoryContract from "../contracts/PortfolioFactory";
+import CreatePortfolioForm from "../components/CreatePortfolioForm";
 
 const Create = () => {
   const { address } = useContext(UserContext);
-  const [name, setName] = useState("Portfolio");
-  const [symbol, setSymbol] = useState("FOLO");
-  const [tokenAddresses, setTokenAddresses] = useState([]);
-  const [percentageHoldings, setPercentageHoldings] = useState([]);
-  const [ownerFee, setOwnerFee] = useState(0);
-
-  // Example input to create a portfolio
-  // "Portfolio","FOLO", ["0xa36085F69e2889c224210F603D836748e7dC0088", "0xd0A1E359811322d97991E03f863a0C30C2cF029C"],[40, 60],100
-  const createPortfolio = () => {
-    console.log(
-      `The variables are: ${name}, ${symbol}, ${tokenAddresses}, ${percentageHoldings}, ${ownerFee}`
-    );
-    portfolioFactoryContract.methods
-      .create(name, symbol, tokenAddresses, percentageHoldings, ownerFee)
-      .send({
-        from: address,
-      })
-      .then((receipt) => {
-        console.log(receipt);
-      });
-  };
 
   return (
     <>
@@ -38,24 +16,11 @@ const Create = () => {
         </header>
         <div className="row">
           <h4 className="block col-2">Your new coin:</h4>
-          <h4 className="block col-1">
-            Add crypto tokens to your coin: Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Magnam modi laudantium autem adipisci
-            quibusdam provident consequatur, ab porro? Deserunt nam distinctio
-            nesciunt quia quibusdam obcaecati, excepturi ea magnam enim minus.
-            Dolore voluptas excepturi modi. Iste quia consequuntur maxime beatae
-            est!
-            <CreateForm />
-            <DropDown />
+          <h4 className="col-1">
+            <CreatePortfolioForm/>
           </h4>
         </div>
       </section>
-      <p>{address}</p>
-      {address && (
-        <button className="btn btn-cta" onClick={createPortfolio}>
-          Create Portfolio
-        </button>
-      )}
       <section>
         <h1>Another section</h1>
         <h2>Space</h2>
