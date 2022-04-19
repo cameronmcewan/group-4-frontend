c60c1e30669943136ccf0d3ce05145bad63009dc
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-04-19 16:47:28 +0100

    get public variable from factory contract

--- portfolio/src/routes/Explore.js
+++ portfolio/src/routes/Explore.js
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
-import PortfolioExplore from "../components/PortfolioExplore";
-// import portfolioFactoryContract from "../contracts/PortfolioFactory";
-import portfolios from "../helpers/portfolios.json";
-import { Link } from "react-router-dom";
-const Explore = props => {
-  
+import PortfolioCard from "../components/PortfolioCard";
+import portfolioFactoryContract from "../contracts/PortfolioFactory";
+
+const Explore = () => {
+  const [portfolioAddresses, setPortfolioAddresses] = useState("");
+
+  const getPortfolioAddressesFromContract = async function () {
+    // Get the first portfolio address from the portfolio factory
+    const address = await portfolioFactoryContract.portfolios(0);
+    setPortfolioAddresses(address);
+  };
+
   return (
     <section>
-      <input type={"text"} className="search" placeholder="Search"></input>
       <h3>
         Search for the name of an existing Portfolio or token to filter the
         results
       </h3>
-      <Link to="/detail">
-        <PortfolioExplore token={portfolios.kovan.first} />
-      </Link>
-      <Link to="/detail">
-        <PortfolioExplore token={portfolios.kovan.third} />
-      </Link>
+      <button onClick={getPortfolioAddressesFromContract}>
+        Get Portfolios
+      </button>
+      <p>{portfolioAddresses}</p>
+      <PortfolioCard />
+      <PortfolioCard />
+      <PortfolioCard />
+      <PortfolioCard />
     </section>
   );
 };
