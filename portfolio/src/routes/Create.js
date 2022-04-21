281a910b86f57115451dc97ed1ad8e460a1bee62
cameronmcewan <cm17521@bristol.ac.uk>
2022-04-21 14:09:49 +0100

    Create page - Added begin section before step 1

--- portfolio/src/routes/Create.js
+++ portfolio/src/routes/Create.js
@@ -3,10 +3,17 @@ import CreatePortfolioForm from '../components/CreatePortfolioForm';
 
 const Create = () => {
 
+    const Begin = useRef(null);
     const Step1 = useRef(null);
     const Step2 = useRef(null);
     const Step3 = useRef(null);
     const FinalStep = useRef(null);
+
+    const goToBegin = () => 
+    window.scrollTo({
+        top: Begin.current.offsetTop,
+        behavior: "smooth"
+    });
 
     const goToStep1 = () => 
         window.scrollTo({
@@ -35,6 +42,19 @@ const Create = () => {
 
   return (
     <>
+    <section ref={Begin}>
+    <h1>Create Your Own ETF Token</h1>
+    <li className="block">
+      <ul><h2>1. Choose Crypto Tokens To Add To Your ETF Token</h2></ul>
+      <ul><h2>2. Name Your ETF Token</h2></ul>
+      <ul><h2>3. Set A Fee For Your ETF Token</h2></ul>
+      <ul><h2>4. Review Your Token And Deploy To The Kovan Testnet</h2></ul>
+    </li>
+    <div className="btn-scroll">
+        <button className="btn btn-cta" onClick={goToStep1}>Begin</button>
+    </div>
+    </section>
+
     <section ref={Step1}>
     {/* <div className={this.state.show == 1 ? 'alertW' : 'creatW not'}><Form></Form></div> */}
     <h2>Step 1</h2>
