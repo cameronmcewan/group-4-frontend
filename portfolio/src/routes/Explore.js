619bdc85affe63285ff9851a52ad05bfb8e2bb7c
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-04-22 12:40:39 +0100

    split portfolio card into summary and detail pages

--- portfolio/src/routes/Explore.js
+++ portfolio/src/routes/Explore.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import PortfolioCard from "../components/PortfolioCard";
+import PortfolioExplore from "../components/PortfolioExplore";
 // import portfolioFactoryContract from "../contracts/PortfolioFactory";
 import portfolios from "../helpers/portfolios.json";
+import { Link } from "react-router-dom";
 
-const Explore = () => {
+const Explore = (props) => {
   // const [portfolioAddresses, setPortfolioAddresses] = useState([]);
   // const getTopPortfolios = async function (numPortfolios) {
   //   const addresses = [];
@@ -15,15 +16,17 @@ const Explore = () => {
 
   return (
     <section>
+      <input type={"text"} className="search" placeholder="Search"></input>
       <h3>
         Search for the name of an existing Portfolio or token to filter the
         results
       </h3>
-      {/* <button className="btn btn-cta" onClick={getTopPortfolios(3)}>
-        Load top portfolios
-      </button> */}
-      <PortfolioCard token={portfolios.kovan.first} />
-      <PortfolioCard token={portfolios.kovan.third} />
+      <Link to="/detail">
+        <PortfolioExplore token={portfolios.kovan.first} />
+      </Link>
+      <Link to="/detail">
+        <PortfolioExplore token={portfolios.kovan.third} />
+      </Link>
     </section>
   );
 };
