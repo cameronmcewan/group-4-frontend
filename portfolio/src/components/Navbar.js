e55234425fb374b8997ae8d78a47d79c2b98bfb6
cameronmcewan <cm17521@bristol.ac.uk>
2022-04-22 13:29:10 +0100

    centered the navbar components, added white logo

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,28 +1,28 @@
 import React from "react";
-import Logo from "../assets/logos/folio-tnr.svg";
+import Logo from "../assets/logos/folio_clean_2.svg";
 import MetaMask from "./MetaMask";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
       <nav>
-        <ul className="topnav">
-          <Link to="/">
-            <img className="logo" src={Logo} alt="The Folio Logo" />
-          </Link>
-          <li>
-            <Link to="explore">Explore</Link>
-          </li>
-          <li>
-            <Link to="create">Create</Link>
-          </li>
-          <li className="right">
-            <MetaMask />
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <Link className="tab" to="explore">Explore</Link>
+            </li>
+            <li>
+              <Link className="logo" to="/">
+                <img  src={Logo} alt="The Folio Logo" />
+              </Link>
+            </li>
+            <li>
+              <Link className="tab" to="create">Create</Link>
+            </li>
+            <li className="right">
+              <MetaMask />
+            </li>
+          </ul>  
       </nav>
-    </div>
   );
 }
 
