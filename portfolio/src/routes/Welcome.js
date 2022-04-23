87de32b93181257f818107ebc098c9ef21e7e354
cameronmcewan <cm17521@bristol.ac.uk>
2022-04-23 15:08:29 +0100

    small changes

--- portfolio/src/routes/Welcome.js
+++ portfolio/src/routes/Welcome.js
@@ -7,12 +7,9 @@ const Welcome = () => {
       <section>
         <h1>Welcome to Folio!</h1>
         <h3>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
-          consectetur vero at, porro rerum laudantium, maxime, doloremque
-          ratione doloribus adipisci inventore nesciunt quae nulla corrupti
-          saepe consequuntur modi magni quod.
+
         </h3>
-        <img src={Logo} alt="logo" />
+        {/* <img src={Logo} alt="logo" /> */}
       </section>
       <section>
         <h1>More stuff</h1>
