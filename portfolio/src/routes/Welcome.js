768f23060e8af98951a17da2ce2793ed868e5e2c
cameronmcewan <cm17521@bristol.ac.uk>
2022-04-25 11:37:08 +0100

    hover effects

--- portfolio/src/routes/Welcome.js
+++ portfolio/src/routes/Welcome.js
@@ -2,17 +2,11 @@ import React from "react";
 import Logo from "../assets/logos/pie-chart-logo.svg";
 
 const Welcome = () => {
+
   return (
     <>
-      <section>
+      <section id="Landing">
         <h1>Welcome to Folio!</h1>
-        <h3>
-
-        </h3>
-        {/* <img src={Logo} alt="logo" /> */}
-      </section>
-      <section>
-        <h1>More stuff</h1>
       </section>
     </>
   );
