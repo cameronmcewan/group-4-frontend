9b95b2021c5282b79c174005b710ae487760ec6c
Prajwayne <46771166+Prajwayne@users.noreply.github.com>
2022-04-27 16:28:38 +0100

    testing for two buttons and they have passed

---
+++ portfolio/src/components/form/test/button.test.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import  ReactDOM  from "react-dom";
+// import MetaMask from "../../MetaMask";
+// import {render} from "@testing-library/react";
+
+import{render} from '@testing-library/react'
+import { Button } from 'antd'
+
+
+// testing on Metamask button tests done on baises of class names 
+describe("Button test", () =>{
+it('Renders with a className equal to the variant', () => {
+    const { container } = render(<Button variant="btn btn-cta" />)
+    expect(container.firstChild) // Check for className here
+});
+});
+
+// testing on Buy button tests done on baises pf class names 
+describe("Button test", () =>{
+    it('Renders with a className equal to the variant', () => {
+        const { container } = render(<Button variant="btn" />)
+        expect(container.firstChild) // Check for className here
+    });
+    });
