518fc5ff13cf41451be9b1a174427d4dec1bfb59
cameronmcewan <cm17521@bristol.ac.uk>
2022-04-27 19:04:36 +0100

    added welcome page

--- portfolio/src/routes/Welcome.js
+++ portfolio/src/routes/Welcome.js
@@ -1,14 +1,28 @@
-import React from "react";
-import Logo from "../assets/logos/pie-chart-logo.svg";
+import React, { useRef } from "react";
+import PortfolioAccordion from "../components/explore/PortfolioAccordion";
+import portfolios from "../helpers/portfolios.json";
+import { Link } from "react-router-dom";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 const Welcome = () => {
 
+
   return (
-    <>
-      <section id="Landing">
-        <h1>Welcome to Folio!</h1>
-      </section>
-    </>
+    <section id="Landing">
+      <h1 className="intro">
+         Index-As-A-Token. Decentralized. Diversified. Crypto.
+      </h1>
+      <div className="row welcome">
+        <Link className="btn-welcome hollow" to="explore">
+          Explore Existing PortFolios
+        </Link>  
+
+        <Link className="btn-welcome hollow" to="create">
+          Create Your Own PortFolio
+        </Link>
+      </div>
+  
+    </section>
   );
 };
 
